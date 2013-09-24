efdd0e6882843e90aff623ed4bf260ae2ea1e9ff
Kristian Ekle <kristian.ekle@gmail.com>
2013-09-24 16:42:22 +0100

    Crontab movement simulation

--- simulation/movement.py
+++ simulation/movement.py
@@ -1,3 +1,11 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 
-#awesomeness wil be created
+import MySQLdb as mdb
+from random import randint
+
+con = mdb.connect('localhost', 'sauadmin', 'dolly', 'prosjektsau')
+cur = con.cursor()
+query = """UPDATE `sauer` SET `hr`=%s, `lat`=%s, `long`=%s;"""
+#Ca midt i Trondheim
+cur.execute(query, ((randint(120, 210)) , ("63." + str(randint(400000, 500000))) , ("10." + str(randint(400000,500000))) ))
+con.commit()
