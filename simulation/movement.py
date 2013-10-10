3e1ebdc86aa27bf254a72d8466ca16ceee17630f
Kristian Ekle <kristian.ekle@gmail.com>
2013-10-10 12:09:10 +0100

    Added respiration and temp simulation

--- simulation/movement.py
+++ simulation/movement.py
@@ -11,8 +11,8 @@ sheep = cur.fetchall()
 
 for she in sheep:
     shee = str(she[0])
-    query = "UPDATE `sauer` SET `hr`=%s, `lat`=%s, `long`=%s WHERE `id`=%s;"
+    query = "UPDATE `sauer` SET `hr`=%s, `lat`=%s, `long`=%s, `temp`=%s, `respiration`=%s  WHERE `id`=%s;"
     #Ca midt i Trondheim
-    cur.execute(query, ((randint(60, 110)) , ("63." + str(randint(400000, 500000))) , ("10." + str(randint(400000,500000))), shee ))
+    cur.execute(query, ((randint(60, 110)) , ("63." + str(randint(400000, 500000))) , ("10." + str(randint(400000,500000))), (38+randint(0,2)), (10+randint(2,8)) , shee ))
 
 con.commit()
